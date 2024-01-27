e00d0325b16e30d7c9720930eab5d36706cad0ac
brendan sudol <brendansudol@gmail.com>
2024-01-27 13:54:56 -0500

    add about page, for testing

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import { Container } from '@/components/Container';
+
+export default async function AboutPage() {
+  return (
+    <Container>
+      <div>testing testing</div>
+    </Container>
+  );
+}
