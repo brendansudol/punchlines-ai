14e7db7d3bb2634383cea6e110992f6960386645
brendan sudol <brendansudol@gmail.com>
2025-06-25 13:57:24 -0400

    tweaks

--- app/account/components/SubscribeButton.tsx
+++ app/account/components/SubscribeButton.tsx
@@ -5,9 +5,10 @@ import { usePay } from '@/hooks/usePay';
 
 export function SubscribeButton() {
   const { handlePay } = usePay();
+
   return (
     <Button size="sm" onClick={handlePay} variant="secondary">
-      Subscribe for unlimited jokes
+      Get unlimited jokes
     </Button>
   );
 }
